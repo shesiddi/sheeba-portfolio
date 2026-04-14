5ec07ad4b65437f071ae8e0094799d6a37cf5dda
Sheeba <sheeba2311@gmail.com>
2026-04-14 11:30:40 +0400

    project data updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
       "Built a responsive portfolio using React, Tailwind CSS, focusing on performance, clean architecture, and reusable components.",
     tech: ["React", "Tailwind CSS", "Vite"],
     github: "https://github.com/shesiddi/sheeba-portfolio",
-    live: "#",
+    live: "https://sheeba-dev.vercel.app/",
   },
   {
     title: "Crypto Dashboard",
@@ -23,4 +23,30 @@ export const projects = [
     github: "https://github.com/shesiddi/crypto-dashboard",
     live: "https://github.com/shesiddi/crypto-dashboard",
   },
+  {
+    title: "Real-Time Trading Feed",
+    description:
+      "A fintech-style dashboard that streams real-time prices of the top 8 digital assets using Binance WebSocket API. Built with Next.js and optimized for performance with buffered updates and responsive design.",
+    tech: [
+      "Next.js",
+      "Binance WebSocket API",
+      "Vercel Deployment",
+      "React Hooks"
+    ],
+    github: "https://github.com/shesiddi/real-time-trading-feed",
+    live: "https://crypto-live-price-feed.vercel.app/",
+  },
+  {
+    title: "Weather App",
+    description:
+      "A simple yet elegant weather application that provides current weather information and forecasts for any location.",
+    tech: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "OpenWeatherMap API"
+    ],
+    github: "https://github.com/shesiddi/weather-app",
+    live: "https://weather-app-js-neon.vercel.app/"
+  }
 ];
